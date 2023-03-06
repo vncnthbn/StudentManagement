b39f8b058f1c8c496e2a98b0cd45c3f62866fb6a
vncnthbn <vincenthabon3@gmail.com>
2023-03-06 14:09:52 +0800

    Add project files.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+namespace StudentManagement.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+            : base(contextOptions)
+        {
+        
+        }  
+
+        //Code- Approach
+        public DbSet<Student> Students { get; set; }    
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public  int Id { get; set; }   
+        public string FirstName { get; set; }
+        public string MidleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNo { get; set; }
+
+    }
+}
